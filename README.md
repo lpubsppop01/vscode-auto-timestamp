b0ff5a27864d41d66a4046c7040a21711d6b31fc
lpubsppop01 <lpubsppop01@gmail.com>
2017-12-03 13:06:19 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# vscode-auto-timestamp
+
+A Visual Studio Code extension that update timestamp field when saving document.
+
+## Features
+
+- Update last modified time field when saving document
+- Fill birth time field if it is empty
+- Each of the fields are detected by condition of settings
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+- `lpubsppop01.autoTimestamp.lineLimit`
+- `lpubsppop01.autoTimestamp.birthTimeStart`
+- `lpubsppop01.autoTimestamp.birthTimeEnd`
+- `lpubsppop01.autoTimestamp.modifiedTimeStart`
+- `lpubsppop01.autoTimestamp.modifiedTimeEnd`
+- `lpubsppop01.autoTimestamp.momentFormat`
+
+## Installation
+
+TODO
+
+## Author
+[lpubsppop01](https://github.com/lpubsppop01)
+
+## License
+[zlib License](https://github.com/lpubsppop01/vscode-auto-timestamp/raw/master/LICENSE.txt)
---
+++ src/extension.ts
@@ -0,0 +1,147 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as moment from 'moment';
+
+export function activate(context: vscode.ExtensionContext) {
+    const config = new ExtensionConfiguration();
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
+        config.onDidChangeConfiguration();
+    }));
+
+    const core = new ExtensionCore(config);
+    context.subscriptions.push(vscode.workspace.onWillSaveTextDocument(e => {
+        core.onWillSaveTextDocument(e);
+    }));
+}
+
+export function deactivate() {
+}
+
+class ExtensionCore {
+
+    private m_config: ExtensionConfiguration;
+
+    public constructor(config: ExtensionConfiguration) {
+        this.m_config = config;
+    }
+
+    public onWillSaveTextDocument(e: vscode.TextDocumentWillSaveEvent) {
+        var edits: vscode.TextEdit[] = [];
+        const lineIndices = this.getIndexRangeUntil(this.m_config.lineLimit, e.document.lineCount);
+        for (const iLine of lineIndices) {
+            const line = e.document.lineAt(iLine);
+
+            const birthTimeRange = this.getTextRangeBetween(line,
+                 this.m_config.birthTimeStart, this.m_config.birthTimeEnd);
+            if (birthTimeRange != null && birthTimeRange.isEmpty) {
+                const stats = fs.statSync(e.document.fileName);
+                const timeStr = moment(stats.birthtime).format(this.m_config.momentFormat);
+                edits.push(vscode.TextEdit.replace(birthTimeRange, timeStr));
+            }
+
+            const modifiedTimeRange = this.getTextRangeBetween(line,
+                 this.m_config.modifiedTimeStart, this.m_config.modifiedTimeEnd);
+            if (modifiedTimeRange != null) {
+                const stats = fs.statSync(e.document.fileName);
+                const timeStr = moment(stats.mtime).format(this.m_config.momentFormat);
+                edits.push(vscode.TextEdit.replace(modifiedTimeRange, timeStr));
+            }
+        }
+        e.waitUntil(Promise.resolve(edits));
+    }
+
+    private getIndexRangeUntil(limit: number, count: number): number[] {
+        const indices: number[] = [];
+        if (limit > 0) {
+            let i = 0;
+            const iMax = Math.min(limit, count);
+            while (i < iMax) {
+                indices.push(i++);
+            }
+        } else {
+            let i = count - 1;
+            const iMin = Math.max(count - limit, 0);
+            while (i >= iMin) {
+                indices.push(i--);
+            }
+        }
+        return indices;
+    }
+
+    private getTextRangeBetween(line: vscode.TextLine, startPattern: RegExp, endPattern): vscode.Range {
+        const startResult = line.text.match(startPattern);
+        if (startResult == null) return null;
+        const iRangeStart = startResult.index + startResult[0].length;
+        const endResult = line.text.substr(iRangeStart).match(endPattern);
+        if (endResult == null) return null;
+        const iRangeEnd = iRangeStart + endResult.index;
+        const startPos = new vscode.Position(line.lineNumber, iRangeStart);
+        var endPos = new vscode.Position(line.lineNumber, iRangeEnd);
+        return new vscode.Range(startPos, endPos);
+    }
+
+}
+
+class ExtensionConfiguration {
+
+    private m_config: vscode.WorkspaceConfiguration;
+
+    public constructor() {
+        this.m_config = vscode.workspace.getConfiguration("lpubsppop01.autoTimestamp");
+    }
+
+    public onDidChangeConfiguration() {
+        this.m_config = vscode.workspace.getConfiguration("lpubsppop01.autoTimestamp");
+        this.m_birthTimeStart = null;
+        this.m_birthTimeEnd = null;
+    }
+
+    private getValue<T>(propertyName: string, defaultValue: T): T {
+        if (this.m_config == null) return defaultValue;
+        const value = this.m_config.get<T>(propertyName);
+        return value != null ? value : defaultValue;
+    }
+
+    public get lineLimit(): number {
+        return this.getValue<number>("lineLimit", 5);
+    }
+
+    private m_birthTimeStart: RegExp;
+    public get birthTimeStart(): RegExp {
+        if (this.m_birthTimeStart == null) {
+            this.m_birthTimeStart = new RegExp(this.getValue<string>("birthTimeStart", "Created: "));
+        }
+        return this.m_birthTimeStart;
+    }
+
+    private m_birthTimeEnd: RegExp;
+    public get birthTimeEnd(): RegExp {
+        if (this.m_birthTimeEnd == null)  {
+            this.m_birthTimeEnd = new RegExp(this.getValue<string>("birthTimeEnd", "$"));
+        }
+        return this.m_birthTimeEnd;
+    }
+
+    private m_modifiedTimeStart: RegExp;
+    public get modifiedTimeStart(): RegExp {
+        if (this.m_modifiedTimeStart == null) {
+            this.m_modifiedTimeStart = new RegExp(this.getValue<string>("modifiedTimeStart", "Last modified: "));
+        }
+        return this.m_modifiedTimeStart;
+    }
+
+    private m_modifiedTimeEnd: RegExp;
+    public get modifiedTimeEnd(): RegExp {
+        if (this.m_modifiedTimeEnd == null) {
+            this.m_modifiedTimeEnd = new RegExp(this.getValue<string>("modifiedTimeEnd", "$"));
+        }
+        return this.m_modifiedTimeEnd;
+    }
+
+    public get momentFormat(): string {
+        return this.getValue<string>("momentFormat", "YYYY/MM/DD HH:mm:ss");
+    }
+
+}
